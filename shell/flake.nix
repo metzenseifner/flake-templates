417aa8bdfe29769f6d46acd138ae79b84aa7cfa1
Jonathan L. Komar <Jonathan.Komar@dynatrace.com>
2025-10-21 14:23:56 +0200

    Update default shell

--- shell/flake.nix
+++ shell/flake.nix
@@ -1,7 +1,8 @@
 {
+  description = "Use `nix develop` or `nix develop -c $SHELL` to activate me.";
   inputs = {
     systems.url = "github:nix-systems/default";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05"; # or release-25.05
   };
 
   outputs =
@@ -12,16 +13,26 @@
   }:
   let
     forEachSystem =
-      f: nixpkgs.lib.genAttrs (import systems) (system: f { pkgs = import nixpkgs { inherit system; }; });
+      f: nixpkgs.lib.genAttrs (import systems) (system: f { inherit system; pkgs = import nixpkgs { inherit system; }; });
   in
   {
     devShells = forEachSystem (
-      { pkgs }:
-      {
-        default = pkgs.mkShellNoCC {
-          packages = with pkgs; [
+      { pkgs, system }:
+      let
+          myPackages = with pkgs; [
             hello
           ];
+          packageNames = builtins.concatStringsSep " " (map (p: p.name) myPackages);
+      in
+      {
+        default = pkgs.mkShellNoCC {
+          packages = myPackages;
+          shellHook = ''
+          # export PATH=$NEWPATH:$PATH
+          echo "🔧 Activated nix shell for system: ${system}"
+          # echo "📦 Available packages: ${packageNames}"
+          echo "ℹ️  Flake: ${self.description}"
+          '';
         };
       }
     );
