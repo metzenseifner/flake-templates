6cd0149776e1e47ebf17cd2ec6e796a7444cabec
Jonathan L. Komar <Jonathan.Komar@dynatrace.com>
2026-02-25 09:35:14 +0100

    Update shell template with available executables and more documentation in description

--- shell/flake.nix
+++ shell/flake.nix
@@ -1,5 +1,43 @@
 {
-  description = "Use `nix develop` or `nix develop -c $SHELL` to activate me.";
+  description = ''
+    Use `nix develop` or `nix develop -c $SHELL` to activate me.
+
+    # Add version-controlled source code to inputs like this:
+    golangci2-src = {
+      url = "github:golangci/golangci-lint?ref=v2.5.0";
+      flake = false;
+    };
+
+    # Ref inputs in outputs like this
+    golangci_lint_v2_from_source = mkGolangciLintFromSourceDerivation {
+      src = inputs.golangci2-src;
+      version = "2.5.0";
+    };
+    # or without function abstraction:
+    golangci_lint_v2_from_source_derivation = pkgs.buildGoModule {
+      pname = "golangci-lint";
+      version = "2.5.0";
+      src = inputs.golangci2-src;
+
+      vendorHash = null;
+
+      subPackages = [ "cmd/golangci-lint" ];
+
+      ldflags = [
+        "-s"
+        "-w"
+        "-X main.version=2.5.0"
+        "-X main.commit=${inputs.golangci2-src.rev or "unknown"}"
+        "-X main.date=1970-01-01"
+      ];
+
+      postInstall = ''
+        mv "$out/bin/golangci-lint" "$out/bin/golangci-lint-v${lib.versions.major "2.5.0"}"
+      '';
+
+      meta.platforms = lib.platforms.unix;
+    };
+  '';
   inputs = {
     systems.url = "github:nix-systems/default";
     nixpkgs.url = "github:nixos/nixpkgs/release-25.05"; # or release-25.05
@@ -31,6 +69,8 @@
           # export PATH=$NEWPATH:$PATH
           echo "🔧 Activated nix shell for system: ${system}"
           echo "📦 Available packages: ${packageNames}"
+          echo "🧑🏼‍💻 Available executables:"
+          echo "$PATH" | tr ':' '\n' | grep '^/nix/store' | xargs -I{} sh -c 'ls -1 "{}" 2>/dev/null || true' | xargs || true
           '';
         };
       }
